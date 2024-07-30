3f25cef18798acbdf560bdea91fb91cc579c1980
Gabriella Oliveira <gabriellaszaolv50@gmail.com>
2024-07-29 22:23:04 -0300

    fazendo a parte de respostas

--- src/components/Questionario/Questionario.jsx
+++ src/components/Questionario/Questionario.jsx
@@ -7,47 +7,44 @@ import IconSwitcher from '../IconSwitcher/IconSwitcher'
 function Questionario({dados}) {
 
   const [icon, setIcon] = React.useState(false);
-  const [resultado, setResultado] = React.useState([]);
+  const [correctAnswser, setCorrectAnswser] = React.useState(0);
+  const [wrongAnswser, setWrongAnswer] = React.useState(0);
   const [currentQ, setCurrentQ] = React.useState(0);
+  const [answerResponse, setAnswerResponse] =  React.useState();
 
 
-  const prevQ = () =>{
-
-    if(currentQ <= dados.length - 1){
-      return setCurrentQ((currentQ) => currentQ  + 1)
-    }
-    setCurrentQ((currentQ) => currentQ - 1)
-  }
-
-  const nextQ = () =>{
-    
-    if(currentQ >= dados.length - 1){
-      return setCurrentQ((currentQ) => currentQ)
-    }
-    setCurrentQ((currentQ) => currentQ + 1)
-
-  }
-
-  
-  // const handleClick = (op, e) =>{    
-  //   setIcon(icon => !icon)
-  //   const result = dados.some((dd)=> op === dd.resposta)
-  //   console.log(result)
-   
-  //   setResultado([...resultado,result])s
-  //   console.log(`Total de resultado das respostas: ${resultado}`)
-    
-  // }
-
-  
   if(dados === undefined) return;
-
-  console.log(dados.length)
   let quizzes;
   quizzes = dados[currentQ]
 
+  //Comparando as respostas do usuário com as respostas corretas
+  const handleAnswer = (q) =>{
 
+    if(q === quizzes.resposta){
+      setAnswerResponse(true)
 
+    }else{
+      setAnswerResponse(false)
+    }
+ }
+
+  console.log( 'Corretas: ' + correctAnswser, 'Erradas: ' + wrongAnswser)
+  const prevQ = () =>{
+    setCurrentQ((currentQ) => currentQ  - 1)
+  }
+  const nextQ = () =>{
+    
+    answerResponse 
+    ? setCorrectAnswser((correctAnswser)  => correctAnswser +  1) 
+    : setWrongAnswer((wrongAnswser)  => wrongAnswser +  1)
+ 
+    if(currentQ !== dados.length - 1) {
+      setCurrentQ((currentQ) => currentQ + 1)
+   }
+   else{
+      console.log('quiz finalizado')
+   }
+ } 
   return (
       <>
        <div className='container-p'>
@@ -57,7 +54,7 @@ function Questionario({dados}) {
             <h2 className='titleQuestion' key={quizzes.pergunta}>{quizzes.pergunta}</h2>
 
             {quizzes.opcoes.map((q , i)=>
-               <p className='responses' key={i}>  { icon ? <CircleCheck size={18}/> : <Circle size={18}/>} {q}</p> 
+               <p  onClick={()=>handleAnswer(q)} className='responses' key={i}>  { icon ? <CircleCheck size={18}/> : <Circle size={18}/>} {q}</p> 
             )}
         </div>
       <div className='link-btn'>
